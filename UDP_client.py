21f85de10486267a58bc274eaee73a4d7326dc02
SpiritOfTheNight <238154775+spiritofthenight@users.noreply.github.com>
2026-02-18 01:59:44 +0100

    Update UDP_client.py

--- UDP_client.py
+++ UDP_client.py
@@ -1,6 +1,24 @@
 import socket
+import sounddevice
+import numpy as np
 import os
-import time
+
+# github : https://github.com/spiritofthenight
+
+IP = "127.0.0.1" # reolace it with your own device's IP
+
+# leave the below port number as it is or replace it with your desired port number 
+PORT = 5000 # (be sure to check your firewall since firewalls may block ports for incoming UDP packets !) 
+
+
+samplerate = 48000
+
+UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDP_socket.bind((IP, PORT))
+
+stream = sounddevice.OutputStream(samplerate=samplerate, channels=1, dtype='float32')
+
+stream.start()
 
 def clear():
     if os.name == 'nt':
@@ -10,31 +28,38 @@ def clear():
     else:
         pass
 
+
+print("Reciever started, waiting for audio...")
+
 def main():
-    UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    IP = "127.0.0.1"
-    PORT = 5000
-    print("\n app is running on ", IP,":", PORT, "...\n")
-    count_down = 60
+    count_down = 1800
+    print("\napp is running on ", IP, ":", PORT, "...\n")
     while True:
         try:
-            msg = input("> ")
-            UDP_socket.sendto(msg.encode(), (IP, PORT))
-            data, addr = UDP_socket.recvfrom(1024)
-            print("Echo:", data.decode())
-            time.sleep(0.5)
+            samplerate = 48000
+            data, addr = UDP_socket.recvfrom(4096)
+
+            pcm16 = np.frombuffer(data, dtype=np.int16)
+
+            audio = pcm16.astype(np.float32) / 32767
+
+            stream.write(audio)
+
+            print("type audio is: ", type(audio))
+            print("Audio is: ", audio)
             count_down -= 1
             if count_down <= 0:
                 clear()
-                count_down = 30
+                count_down = 1800
 
-        except KeyboardInterrupt as k :
-            print("\nyou exited the app with keyboard interruption !", k)
+        except KeyboardInterrupt as k:
+            print("exited ! ", k)
             break
-        
+            
         except Exception as e:
-            print("we ran into a error : ", e)
+            print("Error: ", e)
             break
+
 
 if __name__ == "__main__":
     main()
