e6f113016c614c3fcee07f12ad02b3b58750da8c
SpiritOfTheNight <238154775+spiritofthenight@users.noreply.github.com>
2026-02-18 01:57:28 +0100

    Update UDP_server.py

--- UDP_server.py
+++ UDP_server.py
@@ -2,6 +2,10 @@ import socket
 import soundcard
 import sounddevice
 import numpy as np
+import os
+
+# github:
+# https://github.com/spiritofthenight
 
 def clear():
     if os.name == 'nt':
@@ -18,7 +22,7 @@ def main():
 
     speaker = soundcard.default_speaker()
     loopback = soundcard.get_microphone(id=speaker.id, include_loopback=True)
-    count_down = 60
+    count_down = 1800
 
     with loopback.recorder(samplerate=samplerate) as mic:
         while True:
@@ -30,11 +34,11 @@ def main():
 
             print("audio type is: ", type(audio))
             print("audio is: ", audio)
-            UDP_socekt.sendto(packet, ("127.0.0.1", 5000))
+            UDP_socekt.sendto(packet, ("127.0.0.1", 5000)) # replace it with the IP/PORT number of your destination device
             count_down -= 1
             if count_down <= 0:
                 clear()
-                count_down = 60
+                count_down = 1800
 
 if __name__ == "__main__":
     main()
